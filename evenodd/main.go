892ae95c82a4fe36b731e67d97eb94cc57ad048f
prathikshamp16 <prathikshamp16@gmail.com>
2026-02-09 17:35:54 +0530

    first commit

---
+++ evenodd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Route
+	r.GET("/check/:number", func(c *gin.Context) {
+		numStr := c.Param("number")
+
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Please provide a valid number",
+			})
+			return
+		}
+
+		if num%2 == 0 {
+			c.JSON(http.StatusOK, gin.H{
+				"number": num,
+				"result": "Even",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"number": num,
+				"result": "Odd",
+			})
+		}
+	})
+
+	r.Run(":8080") // start server
+}
